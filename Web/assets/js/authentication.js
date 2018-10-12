99edc3267019b54261c0ea8642343a159efd4de7
Pratna <pratna.meng@hotmail.fr>
2018-10-12 10:56:15 +0200

    update local

--- Web/assets/js/authentication.js
+++ Web/assets/js/authentication.js
@@ -1,7 +1,19 @@
-$('#buttonsignin').click(function() {
+$('#buttonsignin').click(function()
+{
     console.log('button signin clicked');
+    let inputEmail = $('#inputemail').val();
+    let inputPassword = $('#inputpassword').val();
+    if(!inputEmail)
+    {
+        console.log("email is empty");
+    }
+    if(!inputPassword)
+    {
+        console.log("password is password");
+    }
 });
 
-$('#buttonsignup').click(function() {
+$('#buttonsignup').click(function()
+{
     console.log('button signup clicked');
 });
