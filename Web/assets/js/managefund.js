70214ddf59d5a37d01bae51c264951c45f7ec72c
Pratna Meng <pratna.meng@hotmail.fr>
2018-11-05 03:18:24 +0100

    managefund done

--- Web/assets/js/managefund.js
+++ Web/assets/js/managefund.js
@@ -1,4 +1,37 @@
-$('.dropdownitem').click(function() {
-    console.log('dropdownitem ' + $('#dropdownitem').text());
-    $('#dropdowncontent').html($('#dropdownitem').html());
-})
+$(document).ready(function(){
+    $(document).on("keypress", function(event) {
+        if(event.which == 32)
+        {
+            console.log("input : " + $("#fundOperator option:selected").text());
+        }
+    });
+});
+
+$(document).on("click", ".btnModify", function() {
+    let parentcurrentFund = $(this).siblings()[1];
+    let currentFund = parentcurrentFund.innerText;
+    let parentInputs = $(this).siblings()[2];
+    let inputFund = $(parentInputs).find("input:text").val();
+    let inputOperator = $(parentInputs).find("#fundOperator option:selected").text();
+
+    let newFund;
+    switch(inputOperator)
+    {
+        case '+' :
+            newFund = parseFloat(currentFund) + parseFloat(inputFund);
+            $(parentcurrentFund).text(newFund.toString());
+            break;
+        case '-' :
+            newFund = parseFloat(currentFund) - parseFloat(inputFund);
+            if(newFund >= 0)
+            {
+                $(parentcurrentFund).text(newFund.toString());
+            }
+            else
+            {
+                alert("Not enough fund !");
+            }
+            break;
+        default :
+    }
+});
