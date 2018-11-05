f32627b09e5d109c85977259d0d77d4af002bb00
Pratna Meng <pratna.meng@hotmail.fr>
2018-11-05 16:48:36 +0100

    inprogress

--- Web/assets/js/includes.js
+++ Web/assets/js/includes.js
@@ -1,4 +1,6 @@
+var callonce;
 window.onload = function () {
+  callonce = false;
   includeHTML();
 }
 
@@ -15,7 +17,22 @@ function includeHTML() {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
-          if (this.status == 200) {;elmnt.innerHTML = this.responseText;}
+          if (this.status == 200) {
+            let content = this.responseText;
+            $(elmnt).replaceWith(content);
+            let filename = document.location.href.match(/([^\/]+)\..*$/)[1];
+            let array = $("nav>div>ul>li").toArray();           
+            for(let item of array) {
+              if(callonce == true) break;
+              console.log("item : " + item.innerText);
+              if(item.innerText.toLowerCase() == filename.toLowerCase()) {
+                item.classList.add("active");
+                console.log(item.innerHTML + " is active");
+                break;
+              }
+            }
+            callonce = true;
+          }
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /*remove the attribute, and call this function once more:*/
           elmnt.removeAttribute("w3-include-html");
--- Web/assets/js/managefund.js
+++ Web/assets/js/managefund.js
@@ -1,13 +1,38 @@
-$(document).ready(function(){
-    $(document).on("keypress", function(event) {
-        if(event.which == 32)
+$(window).on("keypress", function(event) {
+    if(event.which == 32)
+    {
+        console.log("input : " + $("#fundOperator option:selected").text());   
+        //Find the li and set active
+        $("nav>div>ul>li").each(function()
         {
-            console.log("input : " + $("#fundOperator option:selected").text());
-        }
+            console.log("li name : " + this.innerText);
+        });
+    }
+});
+
+$(window).ready(function() {
+    console.log("ready");
+});
+
+$(window).on("load", function() {
+    console.log("load");
+
+    //Find the li and set active
+    $("nav>div>ul>li").each(function()
+    {
+        console.log("li name : " + this.innerText);
     });
 });
 
-$(document).on("click", ".btnModify", function() {
+$(window).on("focus", function() {
+    console.log("o/");
+});
+
+$(window).on("blur", function() {
+    console.log("o\\");
+});
+
+$(window).on("click", ".btnModify", function() {
     let parentcurrentFund = $(this).siblings()[1];
     let currentFund = parentcurrentFund.innerText;
     let parentInputs = $(this).siblings()[2];
