1ca7bb15bd09ba841085890262ac1bf62929578a
Jean Philippe <bongojeanphilippe@gmail.com>
2018-12-05 12:27:34 +0100

    Construction du component App

---
+++ src/app/gestioncommandes/gestioncommandes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestioncommandes',
+  templateUrl: './gestioncommandes.component.html',
+  styleUrls: ['./gestioncommandes.component.css']
+})
+export class GestioncommandesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
