08a1d1b49ed645fc0b470d50ab5ae503f29eb8dc
Jean Philippe <bongojeanphilippe@gmail.com>
2019-02-25 04:25:28 +0100

    Gestion menu hebodo pour utilisateur

--- ec@ntinebyCUI/src/app/views/menu/list-menu/list-menu.component.ts
+++ ec@ntinebyCUI/src/app/views/menu/list-menu/list-menu.component.ts
@@ -1,6 +1,9 @@
 
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from '../../../services/session.service';
+import { ArticleService } from '../../../services/article.service';
+import { MenuService } from '../../../services/menu.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-list-menu',
@@ -10,13 +13,40 @@ import { SessionService } from '../../../services/session.service';
 export class ListMenuComponent implements OnInit {
 
   readonly today: Date = new Date();
-
-  constructor(private sessionService: SessionService) { }
+public todayData;
+  public articles;
+  public entrees;
+    public entreesToday;
+  public resistances;
+    public resistancesToday;
+  public desserts;
+    public dessertsToday;
+  constructor(private sessionService: SessionService,private articleService: ArticleService) { }
 
   ngOnInit() {
     this.sessionService.weeklyDaysObservable.subscribe(data => this.today.setTime(data.today.getTime()));
+this.initMenu();
+  }
+   capitalize(string:String) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+  initMenu(){
+    
+    var Today = this.capitalize(this.today.toString());
 
-    // display menuArticles of today
+    this.articleService.getMenuByDay("Lundi")
+    .subscribe(data => 
+      { 
+        this.todayData= data; console.log(data);
+         
+        this.articleService.getMenuArticles(this.todayData['_id'],"entree").subscribe(data => { this.entreesToday = data['articles']; console.log(data+"xx");
+         },err => { console.log(err);});
+        this.articleService.getMenuArticles(this.todayData['_id'],"resistance").subscribe(data => { this.resistancesToday = data['articles']; },err => { console.log(err);});
+        this.articleService.getMenuArticles(this.todayData['_id'],"dessert").subscribe(data => { this.dessertsToday = data['articles']; },err => { console.log(err);});
+      },err => { console.log(err);}); 
+
+  
+  
   }
 
 }
